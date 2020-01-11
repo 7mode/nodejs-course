b54d087b3a67df355e10cf4469ed457669dcbbb9
7mode <m.n3294@gmail.com>
2020-01-11 14:05:58 +0200

    es6 Object property shorthand and Object destructure

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -3,8 +3,7 @@ const request = require('request')
 forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/4e2d5ae5bbb9191e2a6b67d790777286/' + latitude + ',' + longitude
 
-    request({url: url, json: true},(error, response) => {
-        const body = response.body;
+    request({ url, json: true },(error, { body }) => {
         if (error) {
             callback('Unable connect to weather service!', undefined)
         } else if (body.error) {
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -2,16 +2,18 @@ const request = require('request')
 
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiN21vZGUiLCJhIjoiY2s1MGt3Z3MwMGlpYjNlbWllbHB4ZmMwbyJ9.V7RJjuMHVfVTG5yUUbWL_w&limit=1'
-    request({ url: url, json: true}, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to location services!', undefined)
-        } else if (response.body.features.length === 0) {
+        } else if (body.features.length === 0) {
             callback('Unable to find location. Try another search!', undefined)
         } else {
+            const longitude = body.features[0].center[0];
+            const latitude = body.features[0].center[1];
             callback(undefined, {
-                longitude: response.body.features[0].center[0],
-                latitude: response.body.features[0].center[1],
-                location: response.body.features[0].place_name
+                longitude,
+                latitude,
+                location: body.features[0].place_name
             })
         }
     })
