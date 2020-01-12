2b5a2f221dd423eb7373591ed8fc1028fa454d64
7mode <m.n3294@gmail.com>
2020-01-12 09:39:25 +0200

    Serving up Static Assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,24 +1,18 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
 
-// base route get
-app.get('', (req, res) => {
-    res.send('Hello Client Side :O')
-})
+const publicDirectoryPath = path.join(__dirname, '../public');
 
-// help route get
-app.get('/help', (req, res) => {
-    res.send('Help Page')
-})
-// about route get
-app.get('/about', (req, res) => {
-    res.send('About page')
-})
+app.use(express.static(publicDirectoryPath))
 
 // help route get
 app.get('/weather', (req, res) => {
-    res.send('weather')
+    res.send({
+        forecast: 'Cloudy',
+        location: 'Tel Aviv'
+    })
 })
 
 const port = 3000;
