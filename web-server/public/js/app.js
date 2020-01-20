a326f2d81a3d110f82106a6417c43e2d14425b5a
7mode <m.n3294@gmail.com>
2020-01-20 10:02:20 +0200

    58. Creating a Search Form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -5,13 +5,21 @@ fetch('http://puzzle.mead.io/puzzle').then((response) => {
         console.log(data)
     })
 })
-fetch('http://localhost:3000/weather?address=Tel%20Aviv').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+    fetch('http://localhost:3000/weather?address='+ location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
     })
 })
