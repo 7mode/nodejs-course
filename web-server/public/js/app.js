ae3475b562bcb9f9e990e932941eff5a9f474716
7mode <m.n3294@gmail.com>
2020-01-20 07:48:28 +0200

    57. Browser HTTP Requests with Fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,17 @@
 console.log('this is javascript FE print')
+
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+    response.json().then((data) => {
+        console.log(data)
+    })
+})
+fetch('http://localhost:3000/weather?address=Tel%20Aviv').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+})
