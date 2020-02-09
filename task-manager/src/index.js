19efd74bcc7a5db7ea5361cdc198ba1f2cb22e58
7mode <m.n3294@gmail.com>
2020-02-09 18:03:08 +0200

    91+92 Resource Reading Endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -12,9 +12,28 @@ app.post('/users', (req, res) => {
     const user = new User(req.body)
 
     user.save().then(() => {
-        res.send(user)
+        res.status(201).send(user)
     }).catch((e) => {
         res.status(400).send(e)
+    })
+})
+
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send(e)
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    User.findById(req.params.id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send(e)
     })
 })
 
@@ -22,9 +41,28 @@ app.post('/tasks', (req, res) => {
     const user = new Task(req.body)
 
     user.save().then(() => {
-        res.send(user)
+        res.status(201).send(user)
     }).catch((e) => {
         res.status(400).send(e)
+    })
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send(e)
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    Task.findById(req.params.id).then((task) => {
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch((e) => {
+        res.status(500).send(e)
     })
 })
 
