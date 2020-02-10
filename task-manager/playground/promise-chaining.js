be8635ae9e469e375431d75d4b7fea52155cb1ee
7mode <m.n3294@gmail.com>
2020-02-10 09:53:58 +0200

    96 Async/Await playground

--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -1,5 +1,6 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
+const Task = require('../src/models/task')
 
 User.findByIdAndUpdate('5e3ec243bc666008a17541d7', { age: 1 }).then((user) => {
     console.log(user)
@@ -7,5 +8,30 @@ User.findByIdAndUpdate('5e3ec243bc666008a17541d7', { age: 1 }).then((user) => {
 }).then((result) => {
     console.log(result)
 }).catch((e) => {
+    console.log(e)
+})
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age: age })
+    const count = await User.countDocuments({ age: age })
+    return count
+}
+
+updateAgeAndCount('5e3ec243bc666008a17541d7', 2).then((count) => {
+    console.log(count)
+}).catch((e) => {
+    console.log(e)
+})
+
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ completed: false })
+    return count
+}
+
+deleteTaskAndCount('some real id').then((count) => {
+    console.log(count)
+}).then((e) => {
     console.log(e)
 })
