45fe6758b9cc2ce12f6e80058c668e0c08b2307d
7mode <m.n3294@gmail.com>
2020-02-10 07:44:01 +0200

    95 Async/Await playground

---
+++ playground/9-async-await.js
@@ -0,0 +1,25 @@
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (a < 0 || b < 0) {
+                reject('Numbers must be non-negative')
+            }
+
+            resolve(a + b)
+        }, 2000)
+    })
+
+}
+
+const doWork = async () => {
+    const sum = await add(7, 5)
+    const sum1 = await add(sum, 4)
+    const sum2 = await add(sum1, -6)
+    return sum2
+}
+
+doWork().then((result) => {
+    console.log('Result', result)
+}).catch((e) => {
+    console.log('Error', e)
+})
